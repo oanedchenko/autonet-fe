bfea5ef3313abdac716641b9daaf78a20dbfe253
Oleg Anedchenko <oanedchenko@gmail.com>
2018-06-24 21:12:04 +0200

    new server vm

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 npm run build
 cd dist
 tar czf fe-auto1.tgz *
-scp fe-auto1.tgz root@bb97.k.time4vps.cloud:/root/deploy/auto1
+scp fe-auto1.tgz root@10f20.k.time4vps.cloud:/root/deploy/auto1
 
-ssh root@bb97.k.time4vps.cloud /root/deploy/auto1/fe-deploy-auto1.sh
+ssh root@10f20.k.time4vps.cloud /root/deploy/auto1/fe-deploy-auto1.sh
